1008a65e9453a7c21ff6239cfcbad7be18c1b430
Samyr Rakoto <Samyr Rakoto>
2022-11-15 18:13:37 +0100

    Created profile menu component

--- src/components/header/ThemeToggler.vue
+++ src/components/header/ThemeToggler.vue
@@ -1,7 +1,7 @@
 <template>
     <!-- Theme toggler -->
     <li class="flex">
-        <button class="rounded-md focus:outline-none focus:shadow-outline-purple" v-on:click="toggleTheme"
+        <button class="rounded-md focus:outline-none focus:shadow-outline-purple" @click="toggleTheme"
             aria-label="Toggle color mode">
             <template x-if="!dark">
                 <svg class="w-5 h-5" aria-hidden="true" fill="currentColor" viewBox="0 0 20 20">
