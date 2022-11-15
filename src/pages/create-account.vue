bf4405450439db130515276c391d42b3291c5f84
Samyr Rakoto <Samyr Rakoto>
2022-11-15 21:46:46 +0100

    Converted more html files to vue components

--- src/pages/create-account.vue
+++ src/pages/create-account.vue
@@ -3,15 +3,15 @@
     <div class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800">
       <div class="flex flex-col overflow-y-auto md:flex-row">
         <div class="h-32 md:h-auto md:w-1/2">
-          <img aria-hidden="true" class="object-cover w-full h-full dark:hidden" src="../assets/img/login-office.jpeg"
-            alt="Office" />
+          <img aria-hidden="true" class="object-cover w-full h-full dark:hidden"
+            src="../assets/img/create-account-office.jpeg" alt="Office" />
           <img aria-hidden="true" class="hidden object-cover w-full h-full dark:block"
-            src="../assets/img/login-office-dark.jpeg" alt="Office" />
+            src="../assets/img/create-account-office-dark.jpeg" alt="Office" />
         </div>
         <div class="flex items-center justify-center p-6 sm:p-12 md:w-1/2">
           <div class="w-full">
             <h1 class="mb-4 text-xl font-semibold text-gray-700 dark:text-gray-200">
-              Login
+              Create account
             </h1>
             <label class="block text-sm">
               <span class="text-gray-700 dark:text-gray-400">Email</span>
@@ -25,11 +25,30 @@
                 class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
                 placeholder="***************" type="password" />
             </label>
+            <label class="block mt-4 text-sm">
+              <span class="text-gray-700 dark:text-gray-400">
+                Confirm password
+              </span>
+              <input
+                class="block w-full mt-1 text-sm dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:text-gray-300 dark:focus:shadow-outline-gray form-input"
+                placeholder="***************" type="password" />
+            </label>
+
+            <div class="flex mt-6 text-sm">
+              <label class="flex items-center dark:text-gray-400">
+                <input type="checkbox"
+                  class="text-purple-600 form-checkbox focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray" />
+                <span class="ml-2">
+                  I agree to the
+                  <span class="underline">privacy policy</span>
+                </span>
+              </label>
+            </div>
 
             <!-- You should use a button here, as the anchor is only used for the example  -->
             <a class="block w-full px-4 py-2 mt-4 text-sm font-medium leading-5 text-center text-white transition-colors duration-150 bg-purple-600 border border-transparent rounded-lg active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple"
-              href="../index.html">
-              Log in
+              href="./login.html">
+              Create account
             </a>
 
             <hr class="my-8" />
@@ -52,15 +71,8 @@
             </button>
 
             <p class="mt-4">
-              <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                href="./forgot-password.html">
-                Forgot your password?
-              </a>
-            </p>
-            <p class="mt-1">
-              <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline"
-                href="./create-account.html">
-                Create account
+              <a class="text-sm font-medium text-purple-600 dark:text-purple-400 hover:underline" href="./login.html">
+                Already have an account? Login
               </a>
             </p>
           </div>
@@ -69,14 +81,3 @@
     </div>
   </div>
 </template>
-
-<script lang="ts">
-export default defineComponent({
-  data() {
-    return {
-      title: 'Login',
-    }
-  },
-});
-</script>
-
--- src/pages/forgot-password.vue
+++ src/pages/forgot-password.vue
@@ -1,21 +1,4 @@
-<!DOCTYPE html>
-<html :class="{ 'theme-dark': dark }" x-data="data()" lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Forgot password - Windmill Dashboard</title>
-    <link
-      href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
-      rel="stylesheet"
-    />
-    <link rel="stylesheet" href="../assets/css/tailwind.output.css" />
-    <script
-      src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.x.x/dist/alpine.min.js"
-      defer
-    ></script>
-    <script src="../assets/js/init-alpine.js"></script>
-  </head>
-  <body>
+<template>
     <div class="flex items-center min-h-screen p-6 bg-gray-50 dark:bg-gray-900">
       <div
         class="flex-1 h-full max-w-4xl mx-auto overflow-hidden bg-white rounded-lg shadow-xl dark:bg-gray-800"
@@ -62,5 +45,4 @@
         </div>
       </div>
     </div>
-  </body>
-</html>
+</template>
