1b4ad01b312a3714e22611c9129f183724988a15
Samyr Rakoto <Samyr Rakoto>
2022-11-16 22:04:57 +0100

    Made menu elements dynamic with active toggles

--- src/components/DesktopSidebar.vue
+++ src/components/DesktopSidebar.vue
@@ -6,96 +6,29 @@
                 Windmill Dashboard
             </NuxtLink>
             <ul class="mt-6">
-                <li class="relative px-6 py-3">
-                    <!-- Active items have the snippet below -->
-                    <!-- <span
+                <span v-for="element in menuElements" :key="element.menu" >
+                <li
+                    class="relative px-6 py-3"
+                    @click="toggleSelectedElement(element);"
+                    >
+                    <span
+                        v-if="element.isActive"
                         class="absolute inset-y-0 left-0 w-1 bg-purple-600 rounded-tr-lg rounded-br-lg"
                         aria-hidden="true"
-                      ></span> -->
-                    <!-- Add this classes to an active anchor (a tag) -->
-                    <!-- text-gray-800 dark:text-gray-100 -->
-                    <NuxtLink class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        to="/">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6">
-                            </path>
-                        </svg>
-                        <span class="ml-4">Dashboard</span>
-                    </NuxtLink>
-                </li>
-            </ul>
-            <ul>
-                <li class="relative px-6 py-3">
+                    ></span>
                     <NuxtLink  class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        to="/forms">
+                    :to="excludeDashboardElement(element.menu)"
+                    :class="{ 'text-gray-800 dark:text-gray-100': element.isActive }">
                         <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
                             stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                d="M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01">
+                            <path v-for="path in element.svgPaths"
+                                :d="path">
                             </path>
                         </svg>
-                        <span class="ml-4">Forms</span>
+                        <span class="ml-4">{{ element.menu.charAt(0).toUpperCase() + element.menu.slice(1) }}</span>
                     </NuxtLink>
                 </li>
-                <li class="relative px-6 py-3">
-                    <NuxtLink class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        to="/cards">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                d="M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10">
-                            </path>
-                        </svg>
-                        <span class="ml-4">Cards</span>
-                    </NuxtLink>
-                </li>
-                <li class="relative px-6 py-3">
-                    <NuxtLink class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        to="/charts">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z"></path>
-                            <path d="M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z"></path>
-                        </svg>
-                        <span class="ml-4">Charts</span>
-                    </NuxtLink>
-                </li>
-                <li class="relative px-6 py-3">
-                    <NuxtLink class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        to="/buttons">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                d="M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122">
-                            </path>
-                        </svg>
-                        <span class="ml-4">Buttons</span>
-                    </NuxtLink>
-                </li>
-                <li class="relative px-6 py-3">
-                    <NuxtLink class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        to="/modals">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path
-                                d="M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z">
-                            </path>
-                        </svg>
-                        <span class="ml-4">Modals</span>
-                    </NuxtLink>
-                </li>
-                <li class="relative px-6 py-3">
-                    <NuxtLink class="inline-flex items-center w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
-                        to="/tables">
-                        <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round"
-                            stroke-linejoin="round" stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                            <path d="M4 6h16M4 10h16M4 14h16M4 18h16"></path>
-                        </svg>
-                        <span class="ml-4">Tables</span>
-                    </NuxtLink>
-                </li>
+                </span>
                 <li class="relative px-6 py-3">
                     <button
                         class="inline-flex items-center justify-between w-full text-sm font-semibold transition-colors duration-150 hover:text-gray-800 dark:hover:text-gray-200"
@@ -162,16 +95,85 @@
 </template>
 
 <script lang="ts">
+type MenuElement = {
+    menu: string,
+    isActive: boolean,
+    svgPaths: string[],
+};
+
 export default defineComponent({
     data() {
         return {
+            menuElements: [
+                {
+                    menu: "dashboard",
+                    isActive: true,
+                    svgPaths: [
+                        "M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6",
+                    ],
+                },
+                {
+                    menu: "forms",
+                    isActive: false,
+                    svgPaths: [
+                        "M9 5H7a2 2 0 00-2 2v12a2 2 0 002 2h10a2 2 0 002-2V7a2 2 0 00-2-2h-2M9 5a2 2 0 002 2h2a2 2 0 002-2M9 5a2 2 0 012-2h2a2 2 0 012 2m-3 7h3m-3 4h3m-6-4h.01M9 16h.01",
+                    ],
+                },
+                {
+                    menu: "cards",
+                    isActive: false,
+                    svgPaths: [
+                        "M19 11H5m14 0a2 2 0 012 2v6a2 2 0 01-2 2H5a2 2 0 01-2-2v-6a2 2 0 012-2m14 0V9a2 2 0 00-2-2M5 11V9a2 2 0 012-2m0 0V5a2 2 0 012-2h6a2 2 0 012 2v2M7 7h10",
+                    ],
+                },
+                {
+                    menu: "charts",
+                    isActive: false,
+                    svgPaths: [
+                        "M11 3.055A9.001 9.001 0 1020.945 13H11V3.055z",
+                        "M20.488 9H15V3.512A9.025 9.025 0 0120.488 9z",
+                    ]
+                },
+                {
+                    menu: "buttons",
+                    isActive: false,
+                    svgPaths: [
+                        "M15 15l-2 5L9 9l11 4-5 2zm0 0l5 5M7.188 2.239l.777 2.897M5.136 7.965l-2.898-.777M13.95 4.05l-2.122 2.122m-5.657 5.656l-2.12 2.122",
+                    ],
+                },
+                {
+                    menu: "modals",
+                    isActive: false,
+                    svgPaths: [
+                        "M8 16H6a2 2 0 01-2-2V6a2 2 0 012-2h8a2 2 0 012 2v2m-6 12h8a2 2 0 002-2v-8a2 2 0 00-2-2h-8a2 2 0 00-2 2v8a2 2 0 002 2z",
+                    ],
+                },
+                {
+                    menu: "tables",
+                    isActive: false,
+                    svgPaths: [
+                        "M4 6h16M4 10h16M4 14h16M4 18h16",
+                    ],
+                },
+            ],
+            previousActiveElement: <MenuElement>{},
             isPagesMenuOpen: false,
+            activeLink: false,
         }
     },
     methods: {
         togglePagesMenu() {
             this.isPagesMenuOpen = !this.isPagesMenuOpen;
         },
+        toggleSelectedElement(menuElement: MenuElement): void {
+            this.menuElements.forEach(menu => {
+                menu.isActive = false;
+            });
+            menuElement.isActive = true;
+        },
+        excludeDashboardElement(menuElement: string): string {
+            return (menuElement === "dashboard" ? "/" : menuElement)
+        }
     },
 });
 </script>
