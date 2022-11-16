d7ceaa52a8c8f50fa109203493c105e631c395b7
Samyr Rakoto <Samyr Rakoto>
2022-11-16 18:24:55 +0100

    Implemented default layout

--- src/pages/index.vue
+++ src/pages/index.vue
@@ -1,21 +1,5 @@
 <template>
-  <!DOCTYPE html>
-  <html x-data="data()" lang="fr">
-    <head>
-      <meta charset="UTF-8" />
-      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-      <title>{{ title }}</title>
-    </head>
-    <body>
-      <div class="flex h-screen bg-gray-50 dark:bg-gray-900">
-        <side-part></side-part>
-        <div class="flex flex-col flex-1">
-          <head-part></head-part>
-          <main-content></main-content>
-        </div>
-      </div>
-    </body>
-  </html>
+  <main-content></main-content>
 </template>
 
 <script lang="ts">
@@ -23,6 +7,7 @@ export default defineComponent({
   data() {
     return {
       title: 'With love',
+
     }
   },
 });
