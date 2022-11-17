35a2c83dd8e23fda3783bade4aa5fc5b14594b26
Samyr Rakoto <Samyr Rakoto>
2022-11-18 00:21:15 +0100

    Implemented components and data to display charts

---
+++ src/components/BarChart.vue
@@ -0,0 +1,51 @@
+<template>
+    <Bar
+      :chart-options="chartOptions"
+      :chart-data="chartData"
+      :chart-id="chartId"
+      :dataset-id-key="datasetIdKey"
+      :plugins="plugins"
+      :css-classes="cssClasses"
+      :styles="styles"
+      :width="width"
+      :height="height"
+    />
+  </template>
+
+<script>
+import { Bar } from 'vue-chartjs'
+import { Chart as ChartJS, Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale } from 'chart.js'
+
+ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
+
+export default defineComponent({
+  name: 'BarChart',
+  components: { Bar },
+  props: {
+    chartId: {
+      type: String,
+      default: 'bar-chart'
+    },
+    width: {
+      type: Number,
+      default: 400
+    },
+    height: {
+      type: Number,
+      default: 400
+    },
+    cssClasses: {
+      default: '',
+      type: String
+    },
+    styles: {
+      type: Object,
+      default: () => {}
+    },
+    plugins: {
+      type: Object,
+      default: () => {}
+    }
+  },
+})
+</script>
--- src/pages/charts.vue
+++ src/pages/charts.vue
@@ -34,7 +34,7 @@
           <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
             Doughnut/Pie
           </h4>
-          <!-- Doughnut chart component here-->
+          <doughnut-chart v-bind="doughnutConfig"></doughnut-chart>
           <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
             <!-- Chart legend -->
             <div class="flex items-center">
@@ -56,7 +56,7 @@
           <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
             Lines
           </h4>
-          <!-- Lines chart component here-->
+          <line-chart v-bind="lineConfig"></line-chart>
           <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
             <!-- Chart legend -->
             <div class="flex items-center">
@@ -74,7 +74,7 @@
           <h4 class="mb-4 font-semibold text-gray-800 dark:text-gray-300">
             Bars
           </h4>
-          <!-- Bar chart component here-->
+          <bar-chart v-bind="barConfig"></bar-chart>>
           <div class="flex justify-center mt-4 space-x-3 text-sm text-gray-600 dark:text-gray-400">
             <!-- Chart legend -->
             <div class="flex items-center">
@@ -93,5 +93,130 @@
 </template>
 
 <script lang="ts">
-export default defineComponent({});
-</script>-
+export default defineComponent({
+  data() {
+    return {
+      barConfig: {
+        type: 'bar',
+        chartData: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+            {
+              label: 'Shoes',
+              backgroundColor: '#0694a2',
+              // borderColor: window.chartColors.red,
+              borderWidth: 1,
+              data: [-3, 14, 52, 74, 33, 90, 70],
+            },
+            {
+              label: 'Bags',
+              backgroundColor: '#7e3af2',
+              // borderColor: window.chartColors.blue,
+              borderWidth: 1,
+              data: [66, 33, 43, 12, 54, 62, 84],
+            },
+          ],
+        },
+        chartOptions: {
+          responsive: true,
+          legend: {
+            display: false,
+          },
+        },
+      },
+      lineConfig: {
+        type: 'line',
+        chartData: {
+          labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+          datasets: [
+            {
+              label: 'Organic',
+              /**
+               * These colors come from Tailwind CSS palette
+               * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+               */
+              backgroundColor: '#0694a2',
+              borderColor: '#0694a2',
+              data: [43, 48, 40, 54, 67, 73, 70],
+              fill: false,
+            },
+            {
+              label: 'Paid',
+              fill: false,
+              /**
+               * These colors come from Tailwind CSS palette
+               * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+               */
+              backgroundColor: '#7e3af2',
+              borderColor: '#7e3af2',
+              data: [24, 50, 64, 74, 52, 51, 65],
+            },
+          ],
+        },
+        chartOptions: {
+          responsive: true,
+          /**
+           * Default legends are ugly and impossible to style.
+           * See examples in charts.html to add your own legends
+           *  */
+          legend: {
+            display: false,
+          },
+          tooltips: {
+            mode: 'index',
+            intersect: false,
+          },
+          hover: {
+            mode: 'nearest',
+            intersect: true,
+          },
+          scales: {
+            x: {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Month',
+              },
+            },
+            y: {
+              display: true,
+              scaleLabel: {
+                display: true,
+                labelString: 'Value',
+              },
+            },
+          },
+        },
+      },
+      doughnutConfig: {
+        type: 'doughnut',
+        chartData: {
+          datasets: [
+            {
+              data: [33, 33, 33],
+              /**
+               * These colors come from Tailwind CSS palette
+               * https://tailwindcss.com/docs/customizing-colors/#default-color-palette
+               */
+              backgroundColor: ['#0694a2', '#1c64f2', '#7e3af2'],
+              label: 'Dataset 1',
+            },
+          ],
+          labels: ['Shoes', 'Shirts', 'Bags'],
+        },
+        chartOptions: {
+          responsive: true,
+          cutoutPercentage: 80,
+          /**
+           * Default legends are ugly and impossible to style.
+           * See examples in charts.html to add your own legends
+           *  */
+          legend: {
+            display: false,
+          },
+        },
+      }
+    }
+  },
+});
+</script>
