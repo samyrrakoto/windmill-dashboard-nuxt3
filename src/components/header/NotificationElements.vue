84a2420ba285339d73c8314b2237c23593050ed8
Samyr Rakoto <Samyr Rakoto>
2022-11-18 14:32:35 +0100

    Refactored notification component

---
+++ src/components/header/NotificationElements.vue
@@ -0,0 +1,38 @@
+<template>
+    <li class="flex" v-for="notification in notifications" :key="notification.type">
+        <NuxtLink class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
+            to="#">
+            <span>{{ notification.type }}</span>
+            <span v-if="notification.amount > 0"
+                class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
+                {{ notification.amount }}
+            </span>
+        </NuxtLink>
+    </li>
+</template>
+<script lang="ts">
+type NotificationMenu = {
+    type: string,
+    amount: number
+}
+export default defineComponent({
+    data() {
+        return {
+            notifications: [
+                {
+                    type: "Sales",
+                    amount: 3
+                },
+                {
+                    type: "Messages",
+                    amount: 13
+                },
+                {
+                    type: "Alerts",
+                    amount: 0
+                },
+            ]
+        }
+    },
+});
+</script>
--- src/components/header/NotificationsMenu.vue
+++ src/components/header/NotificationsMenu.vue
@@ -19,39 +19,13 @@
                 @keydown.escape="closeNotificationsMenu"
                 class="absolute right-0 w-56 p-2 mt-2 space-y-2 text-gray-600 bg-white border border-gray-100 rounded-md shadow-md dark:text-gray-300 dark:border-gray-700 dark:bg-gray-700"
                 aria-label="submenu">
-                <li class="flex">
-                    <NuxtLink class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                        to="#">
-                        <span>Messages</span>
-                        <span
-                            class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
-                            13
-                        </span>
-                    </NuxtLink>
-                </li>
-                <li class="flex">
-                    <NuxtLink class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                        to="#">
-                        <span>Sales</span>
-                        <span
-                            class="inline-flex items-center justify-center px-2 py-1 text-xs font-bold leading-none text-red-600 bg-red-100 rounded-full dark:text-red-100 dark:bg-red-600">
-                            2
-                        </span>
-                    </NuxtLink>
-                </li>
-                <li class="flex">
-                    <NuxtLink class="inline-flex items-center justify-between w-full px-2 py-1 text-sm font-semibold transition-colors duration-150 rounded-md hover:bg-gray-100 hover:text-gray-800 dark:hover:bg-gray-800 dark:hover:text-gray-200"
-                        to="#">
-                        <span>Alerts</span>
-                    </NuxtLink>
-                </li>
+                <header-notification-elements></header-notification-elements>
             </ul>
         </div>
     </li>
 </template>
 
 <script lang="ts">
-
 export default defineComponent({
     data() {
         return {
