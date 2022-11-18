557b7d546ed1ce2372e4fd43875bf5efd1b3bf9a
Samyr Rakoto <Samyr Rakoto>
2022-11-18 12:21:55 +0100

    Installed google font Inter

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,11 +1,18 @@
 // https://v3.nuxtjs.org/api/configuration/nuxt.config
 export default defineNuxtConfig({
-    buildModules: [
-        '@nuxt/typescript-build'
-    ],
     modules: [
         '@nuxtjs/tailwindcss',
     ],
+    app: {
+        head: {
+            link: [
+              {
+                rel: 'stylesheet',
+                href: 'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap'
+              }
+            ]
+        },
+    },
     css: [
         '@/assets/css/tailwind.output.css',
     ],
