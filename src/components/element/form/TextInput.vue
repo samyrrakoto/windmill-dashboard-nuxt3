dc9f9872a9b18d66382d5855495a3aa06ca5699d
Samyr Rakoto <Samyr Rakoto>
2022-11-19 21:43:15 +0100

    Implemented icon and button text input

--- src/components/element/form/TextInput.vue
+++ src/components/element/form/TextInput.vue
@@ -3,18 +3,37 @@
         <span class="text-gray-700 dark:text-gray-400">
             {{ inputLabel }}
         </span>
-        <input v-if="!isTextArea"
-            class="block w-full mt-1 text-sm form-input"
-            :class="textInputClasses"
-            :placeholder="placeHolder" />
-        <textarea v-else
-            class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-textarea focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
-            rows="3"
-            :placeholder="placeHolder"
-        ></textarea>
-        <span class="text-xs dark:text-red-400" :class="inputHelperClasses" v-if="(isInvalid || isValid || hasHint)">
-            {{ inputHelperText }}
-        </span>
+        <div class="relative text-gray-500 focus-within:text-purple-600 dark:focus-within:text-purple-400">
+            <span v-if="!isTextArea">
+                <input
+                    class="block w-full mt-1 text-sm form-input"
+                    :class="textInputClasses, buttonInputClasses"
+                    :placeholder="placeHolder"
+                />
+                <button v-if="(hasButtonLeft || hasButtonRight)"
+                    class="absolute inset-y-0 px-4 text-sm font-medium leading-5 text-white transition-colors duration-150 bg-purple-600 border border-transparent  active:bg-purple-600 hover:bg-purple-700 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
+                    :class="buttonElementClasses"
+                    >
+                    Click
+                </button>
+            </span>
+            <textarea v-else
+                class="block w-full mt-1 text-sm dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 form-textarea focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray"
+                rows="3"
+                :placeholder="placeHolder"
+            ></textarea>
+            <div class="absolute inset-y-0 flex items-center pointer-events-none" :class="iconInputClasses" v-if:="(hasIconLeft || hasIconRight)">
+              <svg class="w-5 h-5" aria-hidden="true" fill="none" stroke-linecap="round" stroke-linejoin="round"
+                stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
+                <path
+                  d="M3 8l7.89 5.26a2 2 0 002.22 0L21 8M5 19h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v10a2 2 0 002 2z">
+                </path>
+              </svg>
+            </div>
+            <span class="text-xs dark:text-red-400" :class="inputHelperClasses" v-if="(isInvalid || isValid || hasHint)">
+                {{ inputHelperText }}
+            </span>
+        </div>
     </label>
 </template>
 
@@ -27,11 +46,25 @@ export default defineComponent({
                 'border-red-600 dark:text-gray-300 dark:bg-gray-700 focus:border-red-400 focus:outline-none focus:shadow-outline-red':this.isInvalid,
                 'border-green-600 dark:text-gray-300 dark:bg-gray-700 focus:border-green-400 focus:outline-none focus:shadow-outline-green':this.isValid,
                 'dark:text-gray-300 dark:border-gray-600 dark:bg-gray-700 focus:border-purple-400 focus:outline-none focus:shadow-outline-purple dark:focus:shadow-outline-gray':this.hasHint,
+                'pl-10': this.hasIconLeft,
+                'pr-10': this.hasIconRight,
             },
             inputHelperClasses: {
                 'text-red-600': this.isInvalid,
                 'text-green-600': this.isValid,
                 'text-gray-600': this.hasHint,
+            },
+            iconInputClasses: {
+                'ml-3': this.hasIconLeft,
+                'right-0  mr-3 ': this.hasIconRight,
+            },
+            buttonInputClasses: {
+                'pl-20 mt-1': this.hasButtonLeft,
+                'pr-20 mt-1': this.hasButtonRight,
+            },
+            buttonElementClasses: {
+                'rounded-l-md': this.hasButtonLeft,
+                'right-0 rounded-r-md': this.hasButtonRight,
             }
         }
     },
@@ -76,6 +109,26 @@ export default defineComponent({
             required: false,
             default: false,
         },
+        hasIconLeft: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        hasIconRight: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        hasButtonLeft: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        hasButtonRight: {
+            type: Boolean,
+            required: false,
+            default: false,
+        }
     }
 })
 </script>
