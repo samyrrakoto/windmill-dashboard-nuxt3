52c445c64691c808ff1c7ab28dfa753ed806ed5f
Samyr Rakoto <Samyr Rakoto>
2022-11-20 22:59:09 +0100

    Fixed forgotten css class on the global div

--- src/layouts/default.vue
+++ src/layouts/default.vue
@@ -1,6 +1,6 @@
 <template>
     <Html lang="en" class="windmill-dashboard" :class="{ 'theme-dark': dark }"></Html>
-    <div class="flex bg-gray-50 dark:bg-gray-900" :class="{'overflow-hidden': isSideMenuOpen }">
+    <div class="flex h-screen bg-gray-50 dark:bg-gray-900" :class="{'overflow-hidden': isSideMenuOpen }">
         <sidebar-content-wrapper></sidebar-content-wrapper>
         <div class="flex flex-col flex-1">
             <header-content-wrapper></header-content-wrapper>
