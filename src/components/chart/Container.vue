ca4dea7ea3ca207b85f56005dfb44bf9d0528671
Samyr Rakoto <Samyr Rakoto>
2022-11-20 14:47:12 +0100

    Created chart container to wrap title and canvas in it

---
+++ src/components/chart/Container.vue
@@ -0,0 +1,21 @@
+<template>
+    <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
+        <chart-title :title="title"></chart-title>
+        <chart-canvas v-bind="chartConfig"></chart-canvas>
+    </div>
+</template>
+
+<script lang="ts">
+export default defineComponent({
+    props: {
+        title: {
+            type: String,
+            required: false,
+            default: ''
+        },
+        chartConfig: {
+            type: Object,
+        }
+    }
+});
+</script>
--- src/pages/charts.vue
+++ src/pages/charts.vue
@@ -2,35 +2,17 @@
   <main class="h-full pb-16 overflow-y-auto">
     <div class="container px-6 mx-auto grid">
       <content-page-title :page-title="'Charts'"></content-page-title>
-      <!-- CTA -->
+
       <element-star-project></element-star-project>
 
       <p class="mb-8 text-gray-600 dark:text-gray-400">
-        Charts are provided by
-        <a class="text-purple-600 dark:text-purple-400 hover:underline" to="https://www.chartjs.org/">
-          Chart.js
-        </a>
-        . Note that the default legends are disabled and you should
-        provide a description for your charts in HTML. See source code for
-        examples.
+        Charts are provided by <a class="text-purple-600 dark:text-purple-400 hover:underline" to="https://www.chartjs.org/">Chart.js</a>. Note that the default legends are disabled and you should provide a description for your charts in HTML. See source code forexamples.
       </p>
 
       <div class="grid gap-6 mb-8 md:grid-cols-2">
-        <!-- Doughnut/Pie chart -->
-        <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-          <chart-title :title="'Doughtnut / Pie'"></chart-title>
-          <chart-canvas v-bind="doughnutConfig"></chart-canvas>
-        </div>
-        <!-- Lines chart -->
-        <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-          <chart-title :title="'Lines'"></chart-title>
-          <chart-canvas v-bind="lineConfig"></chart-canvas>
-        </div>
-        <!-- Bars chart -->
-        <div class="min-w-0 p-4 bg-white rounded-lg shadow-xs dark:bg-gray-800">
-          <chart-title :title="'Bars'"></chart-title>
-          <chart-canvas v-bind="barConfig"></chart-canvas>
-        </div>
+        <chart-container :title="'Doughtnut / Pie'" :chart-config="doughnutConfig"></chart-container>
+        <chart-container :title="'Line'" :chart-config="lineConfig"></chart-container>
+        <chart-container :title="'Bar'" :chart-config="barConfig"></chart-container>
       </div>
     </div>
   </main>
