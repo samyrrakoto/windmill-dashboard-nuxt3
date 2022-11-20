3c4cbdc1f80ef1254ce0565af161867a0794507e
Samyr Rakoto <Samyr Rakoto>
2022-11-20 14:13:57 +0100

    And since all charts are a single component, no need for an external prop

--- src/components/chart/Canvas.vue
+++ src/components/chart/Canvas.vue
@@ -23,7 +23,42 @@ ChartJS.register(Title, Tooltip, Legend, BarElement, CategoryScale, LinearScale)
 export default defineComponent({
   name: 'Canvas',
   props: {
-    ...chartProps,
+    chartOptions: {
+        type: Object
+    },
+    chartData: {
+        type: Object
+    },
+    datasetIdKey: {
+        type: String
+    },
+    chartId: {
+        type: String,
+        default: ''
+    },
+    width: {
+        type: Number,
+        default: 400
+    },
+    height: {
+        type: Number,
+        default: 400
+    },
+    cssClasses: {
+        default: '',
+        type: String
+    },
+    styles: {
+        type: Object,
+        default: () => {}
+    },
+    plugins: {
+        type: Object,
+        default: () => {}
+    },
+    chartType: {
+        type: String,
+    }
   },
   components: {
     Doughnut,
