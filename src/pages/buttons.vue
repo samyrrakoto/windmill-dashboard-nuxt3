501672fe4053035765676d1e5a10afa7187c070a
Samyr Rakoto <Samyr Rakoto>
2022-11-21 13:18:08 +0100

    Fixed buttons display on mobile

--- src/pages/buttons.vue
+++ src/pages/buttons.vue
@@ -7,21 +7,21 @@
     <content-section-title :section-title="'Sizes'"></content-section-title>
     <div class="flex flex-col flex-wrap mb-4 space-y-4 md:flex-row md:items-end md:space-x-4">
       <element-button :button-text="'Block level'" :size="'larger'" :is-block-level="true"></element-button>
-      <element-button :button-text="'Larger'" :size="'larger'"></element-button>
-      <element-button :button-text="'Large'" :size="'large'"></element-button>
-      <element-button :button-text="'Regular'" :size="'regular'"></element-button>
-      <element-button :button-text="'Small'" :size="'small'"></element-button>
-      <element-button :button-text="'Disabled Regular'" :size="'regular'" :is-disabled="true"></element-button>
+      <div><element-button :button-text="'Larger'" :size="'larger'"></element-button></div>
+      <div><element-button :button-text="'Large'" :size="'large'"></element-button></div>
+      <div><element-button :button-text="'Regular'" :size="'regular'"></element-button></div>
+      <div><element-button :button-text="'Small'" :size="'small'"></element-button></div>
+      <div><element-button :button-text="'Disabled Regular'" :size="'regular'" :is-disabled="true"></element-button></div>
     </div>
 
     <!-- Buttons with icons -->
     <content-section-title :section-title="'Icons'"></content-section-title>
     <div class="flex flex-col flex-wrap mb-8 space-y-4 md:flex-row md:items-end md:space-x-4">
-        <element-button :button-text="'Icon right'" :size="'regular'" :has-icon-right="true" :icon-type="'heart'" :is-svg-even-odd="true"></element-button>
-        <element-button :button-text="'Icon left'" :size="'regular'" :has-icon-left="true" :icon-type="'heart'" :is-svg-even-odd="true"></element-button>
-        <element-button :size="'icon-only'" :icon-only="true" :icon-type="'heart'" :is-svg-even-odd="true"></element-button>
-        <element-button :size="'icon-only'" :icon-only="true" :icon-type="'pencil'" :is-rounded="true"></element-button>
-        <element-button :size="'icon-only'" :icon-only="true" :icon-type="'card'" :is-rounded="true" :is-disabled="true"></element-button>
+        <div><element-button :button-text="'Icon right'" :size="'regular'" :has-icon-right="true" :icon-type="'heart'" :is-svg-even-odd="true"></element-button></div>
+        <div><element-button :button-text="'Icon left'" :size="'regular'" :has-icon-left="true" :icon-type="'heart'" :is-svg-even-odd="true"></element-button></div>
+        <div><element-button :size="'icon-only'" :icon-only="true" :icon-type="'heart'" :is-svg-even-odd="true"></element-button></div>
+        <div><element-button :size="'icon-only'" :icon-only="true" :icon-type="'pencil'" :is-rounded="true"></element-button></div>
+        <div><element-button :size="'icon-only'" :icon-only="true" :icon-type="'card'" :is-rounded="true" :is-disabled="true"></element-button></div>
     </div>
   </div>
 </template>
