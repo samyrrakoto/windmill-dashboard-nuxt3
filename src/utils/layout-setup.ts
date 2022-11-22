fd3a399abee1e48032792e05dd148465e1d95958
Samyr Rakoto <Samyr Rakoto>
2022-11-22 18:08:42 +0100

    De factored the layouts setup as it provoked a bug, which fixed it

--- src/utils/layout-setup.ts
+++
@@ -1,8 +0,0 @@
-import { useLayoutStore } from '@/stores/LayoutStore';
-import { mapState } from 'pinia';
-
-export default defineComponent({
-    computed: {
-        ...mapState(useLayoutStore, ['dark', 'isMobileSideMenuOpen'])
-    }
-});
