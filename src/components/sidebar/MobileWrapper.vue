a33ccfae3676f2325a46fc337fd4114af9e21186
Samyr Rakoto <Samyr Rakoto>
2022-11-24 20:01:33 +0100

    Implemented closing mobile menu on click on backdrop

--- src/components/sidebar/MobileWrapper.vue
+++ src/components/sidebar/MobileWrapper.vue
@@ -2,7 +2,8 @@
     <!-- Mobile sidebar -->
     <div>
         <transition name="menu-backdrop">
-            <div class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center" v-if="isMobileSideMenuOpen"></div>
+            <div class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center" v-if="isMobileSideMenuOpen"
+            @click="closeMobileSideMenu"></div>
         </transition>
         <transition name="menu">
             <aside class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden" v-if="isMobileSideMenuOpen">
