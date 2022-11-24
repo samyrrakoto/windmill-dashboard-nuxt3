4b36d1bac0e67486071269c9b9c160a436acc434
Samyr Rakoto <Samyr Rakoto>
2022-11-24 18:30:05 +0100

    Implemented mobile menu transition and change of icon when opened

--- src/components/header/MobileHamburger.vue
+++ src/components/header/MobileHamburger.vue
@@ -1,16 +1,22 @@
 <template>
     <!-- Mobile hamburger -->
     <button class="p-1 -ml-1 mr-5 rounded-md md:hidden focus:outline-none focus:shadow-outline-purple"
-        @click="toggleMobileSideMenu()" aria-label="Menu" v-if="!isMobileSideMenuOpen">
-        <element-svg aria-hidden="true" :class="'w-6 h-6'" :view-box="'0 0 20 20'" :fill="'currentColor'" :pic="'hamburgerMenu'" />
+        @click="toggleMobileSideMenu()" aria-label="Menu">
+        <element-svg aria-hidden="true" :class="'w-6 h-6'" :view-box="'0 0 20 20'" :fill="'currentColor'" :pic="updatePic" />
     </button>
 </template>
 
 <script lang="ts">
 import { useLayoutStore } from '@/stores/LayoutStore';
 export default defineComponent({
+    data() {
+        return {
+            pic: '',
+        }
+    },
     computed: {
         ...mapState(useLayoutStore, ['isMobileSideMenuOpen']),
+        updatePic(){return this.pic = (this.isMobileSideMenuOpen ? 'arrowLeft' : 'hamburgerMenu')}
     },
     methods: {
         ...mapActions(useLayoutStore, ['toggleMobileSideMenu','closeMobileSideMenu'])
--- src/components/sidebar/MobileWrapper.vue
+++ src/components/sidebar/MobileWrapper.vue
@@ -1,20 +1,28 @@
 <template>
     <!-- Mobile sidebar -->
     <div>
-        <div v-if="isMobileSideMenuOpen" x-transition:enter="transition ease-in-out duration-150"
-            x-transition:enter-start="opacity-0" x-transition:enter-end="opacity-100"
-            x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0"
-            class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center"></div>
-        <aside class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden"
-            v-if="isMobileSideMenuOpen" x-transition:enter="transition ease-in-out duration-150"
-            x-transition:enter-start="opacity-0 transform -translate-x-20" x-transition:enter-end="opacity-100"
-            x-transition:leave="transition ease-in-out duration-150" x-transition:leave-start="opacity-100"
-            x-transition:leave-end="opacity-0 transform -translate-x-20"
-            v-click-outside="closeMobileSideMenu"
-            >
-            <sidebar-menu-wrapper />
-        </aside>
+        <transition
+            enter-active-class="transition ease-in-out duration-150"
+            enter-from-class="opacity-0"
+            enter-to-class="opacity-100"
+            leave-active-class="transition ease-in-out duration-150"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0"
+        >
+            <div class="fixed inset-0 z-10 flex items-end bg-black bg-opacity-50 sm:items-center sm:justify-center" v-if="isMobileSideMenuOpen"></div>
+        </transition>
+        <transition
+            enter-active-class="transition ease-in-out duration-50"
+            enter-from-class="opacity-0 transform -translate-x-20"
+            enter-to-class="opacity-100"
+            leave-active-class="transition ease-in-out duration-150"
+            leave-from-class="opacity-100"
+            leave-to-class="opacity-0 transform -translate-x-20"
+        >
+            <aside class="fixed inset-y-0 z-20 flex-shrink-0 w-64 mt-16 overflow-y-auto bg-white dark:bg-gray-800 md:hidden" v-if="isMobileSideMenuOpen">
+                <sidebar-menu-wrapper />
+            </aside>
+        </transition>
     </div>
 </template>
 
